5dbd6c0f910ec461b82092e877e38a97e58ab438
Unknown <haffez23@gmail.com>
2018-11-15 10:21:34 +0100

    Player Controller C#

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public enum GameState { MENU,GAME,GAMEOVER}
+    public static GameState gameState;
+
+    public snakeMouvement SM;
+    public blockManager BM;
+
+    public CanvasGroup MENU_CG;
+    public CanvasGroup GAME_CG;
+    public CanvasGroup GAMEOVER_CG;
+
+    public GUIText scoreText;
+    public GUIText MenuScoreText;
+    public GUIText BestScoreText;
+    public static int SCORE;
+    public static int BESTSCORE;
+
+    bool speedAdded;
+
+    private void Start()
+    {
+        SetMenu();
+        SCORE = 0;
+        speedAdded = false;
+        BESTSCORE = PlayerPrefs.GetInt("BESTSCORE");
+
+    }
+
+    private void Update()
+    {
+        scoreText.text = SCORE + "";
+        MenuScoreText.text = SCORE + "";
+        if(SCORE > BESTSCORE)
+        {
+            BESTSCORE = SCORE;
+            BestScoreText.text = BESTSCORE + "";
+            if(!speedAdded && SCORE > 150)
+            {
+                SM.speed++;
+                speedAdded = true;
+            }
+        }
+
+    }
+
+    public void setMenu()
+    {
+        gameState = GameState.MENU;
+
+        EnableCG(MENU_CG);
+        DisableCG(GAME_CG);
+        DisableCG(GAMEOVER_CG);
+    }
+
+    public void SetGameover()
+    {
+        gameState = GameState.GAMEOVER;
+        EnableCG(MENU_CG);
+        DisableCG(GAME_CG);
+        DisableCG(GAMEOVER_CG);
+
+        foreach (GameObject g in GameObject.FindGameObjectsWithTag("Box"))
+        {
+            Destroy(g);
+        }
+        foreach (GameObject g in GameObject.FindGameObjectsWithTag("Bar"))
+        {
+            Destroy(g);
+        }
+        foreach (GameObject g in GameObject.FindGameObjectsWithTag("SimpleBox"))
+        {
+            Destroy(g);
+        }
+        foreach (GameObject g in GameObject.FindGameObjectsWithTag("Snake"))
+        {
+            Destroy(g);
+        }
+        SM.SpawnBodyPart();
+        BM.SetPreviousPosAfterGameover();
+        speedAdded = false;
+        SM.speed = 3;
+        PlayerPrefs.SetInt("BESTSCORE", BESTSCORE);
+        BM.SimpleBoxPosition.Clear();
+
+    }
+
+    public void EnableCG(CanvasGroup cg)
+    {
+        cg.alpha = 1;
+        cg.blocksRaycasts = true;
+        cg.interactable = true;
+
+    }
+    public void DisableCG(CanvasGroup cg)
+    {
+        cg.alpha = 1;
+        cg.blocksRaycasts = false;
+        cg.interactable = false;
+
+    }
+}
---
+++ Assets/Scripts/blockManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blockManager : MonoBehaviour {
+
+    public snakeMouvement SM;
+    public float distanceSnakeBarrier;
+
+    public GameObject BlockPrefab;
+
+    public float minSpawnTime;
+    public float maxSpawnTime;
+    private float thisTime;
+    private float randomTime;
+
+    public int minSpawnDist;
+    Vector2 previousSnakePos;
+    public List<Vector3> SimpleBoxPositions = new Boo.Lang.List<Vector3>();
+
+
+    // Use this for initialization
+    void Start () {
+        thisTime = 0;
+        //spawnBarrier();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
